59e562780dddfcfcd168dd6cb7e4d4950f70f6b4
Gee <sales.linglong@gmail.com>
2015-08-30 10:40:38 +0700

    Start Day2

--- app/src/main/java/app/gee/geetraffic/MainActivity.java
+++ app/src/main/java/app/gee/geetraffic/MainActivity.java
@@ -1,9 +1,13 @@
 package app.gee.geetraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -28,6 +32,22 @@ public class MainActivity extends AppCompatActivity {
     }//Main Method
 
     private void buttonController() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Sound Effect
+                MediaPlayer buttonPlayer = MediaPlayer.create(getBaseContext(), R.raw.phonton1);
+                buttonPlayer.start();
+
+                //Intent to WebView
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("http://www.263.net/"));
+                startActivity(objIntent);
+
+            }// Event
+        });
 
     }// buttonController
 
