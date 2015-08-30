64a08c8a68bee8b65162542f6c29f347e263b4ba
Gee <sales.linglong@gmail.com>
2015-08-30 12:26:38 +0700

    Adater Finish

---
+++ app/src/main/java/app/gee/geetraffic/MyAdapter.java
@@ -0,0 +1,59 @@
+package app.gee.geetraffic;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by TC on 8/30/2015.
+ */
+public class MyAdapter extends BaseAdapter{
+
+    //Explicit
+    private Context objContext;
+    private int[] iconInts;
+    private String[] titleStrings;
+
+    public MyAdapter(Context objContext, int[] iconInts, String[] titleStrings) {
+        this.objContext = objContext;
+        this.iconInts = iconInts;
+        this.titleStrings = titleStrings;
+    }// Constructor
+
+    @Override
+    public int getCount() {
+        return titleStrings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View objView1 = objLayoutInflater.inflate(R.layout.traffic_listview,viewGroup,false);
+
+        //Show Icon
+        ImageView icoImageView = (ImageView) objView1.findViewById(R.id.imvShowIcon);
+        icoImageView.setImageResource(iconInts[i]);
+
+        //Show Title
+        TextView titleTextView = (TextView) objView1.findViewById(R.id.txtShowTitle);
+        titleTextView.setText(titleStrings[i]);
+
+        return objView1;
+    }
+}// Main Class
